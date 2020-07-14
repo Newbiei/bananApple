f7a4416cb42327fa7f3c79d26a473c5118fe6389
newwbbie <1098160709@qq.com>
2020-07-14 23:16:56 +0800

    新增发送邮件接口

--- src/main/java/com/bananApple/basic/controller/MainController.java
+++ src/main/java/com/bananApple/basic/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.bananApple.basic.controller;
 
 import com.bananApple.basic.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +19,12 @@ public class MainController {
 
     @Resource
     private MainService mainService;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
 
     @RequestMapping("/welcome")
     public String welcome () {
@@ -37,5 +47,22 @@ public class MainController {
     public List getUUEInfo () {
         List list = mainService.getUUEInfo();
         return list;
+    }
+
+    @ResponseBody
+    @RequestMapping("/sendEmail")
+    public String sendEmail() {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo("1098160709@qq.com"); // 接收地址,可传入数组进行群发
+            message.setSubject("测试标题"); // 标题
+            message.setText("测试内容\n这是第二行"); // 内容
+            javaMailSender.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 }
